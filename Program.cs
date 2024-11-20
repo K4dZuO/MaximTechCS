997bf856f2a5b117a1fa2177f507a75dce204973
Ershov Maxim <erschofogt@gmail.com>
2024-11-20 14:21:29 +0500

    Task 6: Added using random nums with API

--- Program.cs
+++ Program.cs
@@ -1,13 +1,80 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace MaximTechC_
 {
+    class APIClass
+    {
+        private static string GetAPIKey()
+        {
+            // Создаем конфигурацию
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Указываем текущую директорию
+                .AddJsonFile("F:\\K4dZuO\\Important for studying\\MaximTech\\MaximTechC#\\config.json", optional: false, reloadOnChange: true) // Подключаем JSON-файл
+                .Build();
+
+            string apiKey = config["ApiKey"];
+            return apiKey;
+        }
+
+        public static async Task<int> GetRandomNumberAsync(int max)
+        {
+            string apiKey = GetAPIKey(); // Получаем API-ключ
+            string url = "https://api.random.org/json-rpc/4/invoke";
+
+            // Формируем JSON-запрос
+            string jsonRequest = $@"
+        {{
+            ""jsonrpc"": ""2.0"",
+            ""method"": ""generateIntegers"",
+            ""params"": {{
+                ""apiKey"": ""{apiKey}"",
+                ""n"": 1,
+                ""min"": 0,
+                ""max"": {max-1}
+            }},
+            ""id"": 1
+        }}";
+
+            using HttpClient client = new HttpClient();
+            try
+            {
+                // Отправляем запрос
+                HttpResponseMessage response = await client.PostAsync(url,
+                    new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json"));
+
+                response.EnsureSuccessStatusCode(); // Выбрасывает исключение, если статус-код неуспешный
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                // Обработка ответа
+                var result = JsonSerializer.Deserialize<JsonElement>(responseContent);
+                int randomNumber = result.GetProperty("result")
+                                          .GetProperty("random")
+                                          .GetProperty("data")[0]
+                                          .GetInt32();
+
+                return randomNumber; // Возвращаем случайное число
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"\nОшибка при запросе к API: {ex.Message}");
+
+                // Генерация случайного числа средствами .NET в случае ошибки
+                Random random = new Random();
+                return random.Next(0, max); // Генерируем случайное число от 0 до 100
+            }
+        }
+    }
 
     class Node
     {
@@ -326,7 +393,7 @@ namespace MaximTechC_
             }
             else
             {
-                sortedAsciiCodes = new int[] { 1, 2, 3, 4 };
+                sortedAsciiCodes = new int[] {1};
             }
 
             Console.Write("\nОтсортированная строка: ");
@@ -334,6 +401,12 @@ namespace MaximTechC_
             {
                 Console.Write(Convert.ToChar(code));
             }
+
+            // task 6
+            int randomIndex = APIClass.GetRandomNumberAsync(res.Length).GetAwaiter().GetResult(); // Синхронный вызов
+            Console.WriteLine($"\nСлучайное число: {randomIndex}");
+            res = res.Remove(randomIndex, 1);
+            Console.WriteLine($"Обработанная строка без символа на {randomIndex}-ом индексе: {res}");
         }
     }
 }
